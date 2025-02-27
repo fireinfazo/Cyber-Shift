691631bb293181f50124b4a704430e8e109f766d
BlizGame <kirilmalyy@gmail.com>
2025-02-27 11:49:58 +0200

    Temp Commit
    Если что-то сломаю в проекте, откачусь сюда (Начинаю делать анимации, уже есть колайдеры на карте)

---
+++ Cyber-Shift/Assets/Scripts/gun/Shotgun.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private int bulletCount = 10;
+    [SerializeField] private float spreadAngle = 10f;
+    [SerializeField] private float verticalSpreadAngle = 5f;
+    [SerializeField] private float bulletSpeed = 20f;
+    [SerializeField] private float bulletLifetime = 2f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireShotgun();
+        }
+    }
+
+    void FireShotgun()
+    {
+        for (int i = 0; i < bulletCount; i++)
+        {
+            float horizontalOffset = Random.Range(-spreadAngle, spreadAngle);
+            float verticalOffset = Random.Range(-verticalSpreadAngle, verticalSpreadAngle);
+            Quaternion spreadRotation = Quaternion.Euler(verticalOffset, horizontalOffset, 0);
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation * spreadRotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = bullet.transform.forward * bulletSpeed;
+            }
+            Destroy(bullet, bulletLifetime);
+        }
+    }
+}
