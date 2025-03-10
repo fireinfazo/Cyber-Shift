1a701e4215cc37bd9326c1e77135dfbf8f5c6a6d
BlizGame <kirilmalyy@gmail.com>
2025-03-10 23:31:03 +0200

    Стрельба V1 + Двери

---
+++ Cyber-Shift/Assets/Scripts/Logic/Enemy/EnemyHealth.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;  // Для использования корутин
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public float armorPercentage = 0f;
+    private int currentHealth;
+
+    public AudioClip hitSound;  
+    public AudioClip deathSound;  
+    private AudioSource audioSource; 
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        audioSource = GetComponent<AudioSource>();  
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            TakeDamage(10);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        int reducedDamage = Mathf.RoundToInt(damage * (1f - armorPercentage / 100f));
+        currentHealth -= reducedDamage;
+
+        Debug.Log($"Враг получил {reducedDamage} урона. Осталось здоровья: {currentHealth}");
+
+
+        if (audioSource != null && hitSound != null)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Враг уничтожен");
+
+        if (audioSource != null && deathSound != null)
+        {
+            audioSource.PlayOneShot(deathSound);
+        }
+
+        StartCoroutine(DestroyAfterSound());
+    }
+
+    IEnumerator DestroyAfterSound()
+    {
+        yield return new WaitForSeconds(deathSound.length);
+
+        Destroy(gameObject);
+    }
+}
--- Cyber-Shift/Assets/Scripts/gun/Shotgun.cs
+++ Cyber-Shift/Assets/Scripts/gun/Shotgun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class Shotgun : MonoBehaviour
 {
@@ -10,17 +11,50 @@ public class Shotgun : MonoBehaviour
     [SerializeField] private float verticalSpreadAngle = 5f;
     [SerializeField] private float bulletSpeed = 20f;
     [SerializeField] private float bulletLifetime = 0.5f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private TextMeshProUGUI ammoText;
+
+    private int ammo = 6;
+    private bool canShoot = true;
+    private bool isReloading = false;
+
+    void Start()
+    {
+        UpdateAmmoUI();
+    }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canShoot && !isReloading && ammo > 0)
         {
             FireShotgun();
-            weaponSource.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && ammo < 6 && canShoot && !isReloading)
+        {
+            Reload();
         }
     }
 
     void FireShotgun()
+    {
+        canShoot = false;
+        animator.ResetTrigger("Fire");
+        animator.SetTrigger("Fire");
+        weaponSource.Play();
+        ammo--;
+        UpdateAmmoUI();
+    }
+
+    void Reload()
+    {
+        canShoot = false;
+        isReloading = true;
+        animator.ResetTrigger("Reload");
+        animator.SetTrigger("Reload");
+    }
+
+    public void OnFireAnimationEvent()
     {
         for (int i = 0; i < bulletCount; i++)
         {
@@ -34,6 +68,31 @@ public class Shotgun : MonoBehaviour
                 rb.velocity = bullet.transform.forward * bulletSpeed;
             }
             Destroy(bullet, bulletLifetime);
+        }
+    }
+
+    public void OnReloadAnimationEvent()
+    {
+        ammo = 6;
+        UpdateAmmoUI();
+    }
+
+    public void OnReloadAnimationEnd()
+    {
+        canShoot = true;
+        isReloading = false;
+    }
+
+    public void OnFireAnimationEnd()
+    {
+        canShoot = true;
+    }
+
+    void UpdateAmmoUI()
+    {
+        if (ammoText != null)
+        {
+            ammoText.text = "Ammo: " + ammo;
         }
     }
 }
